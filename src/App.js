2d82d2b23f6a9fc440ddf00fc795d3a370b74f0d
Ujjwal <Ujjwalbhatt04@gmail.com>
2022-08-06 22:41:30 +0530

    rrg

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+let name="Ujjwal";
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore nam accusantium quam in voluptas veniam suscipit maiores quas animi, voluptate dolorum aperiam sequi qui, alias reiciendis doloremque? Officiis sint exercitationem inventore iste maxime?</h1>
+    </div>
+    </>
+  );
+}
+export default App;
+  
