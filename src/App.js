a4ad4186921d2033646d7f7e7dc5330f2e61d7e1
Ujjwal <Ujjwalbhatt04@gmail.com>
2022-08-07 10:59:53 +0530

    textutils

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 
 import './App.css';
-let name="Ujjwal";
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    
-    <div className="container">
-      <h1>Hello {name}</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore nam accusantium quam in voluptas veniam suscipit maiores quas animi, voluptate dolorum aperiam sequi qui, alias reiciendis doloremque? Officiis sint exercitationem inventore iste maxime?</h1>
-    </div>
+      <Navbar title= "Texttutils"/>
+      {/* <Navbar/> */}
     </>
+
   );
 }
 export default App;
-  
---
+++ src/module1.mjs
@@ -0,0 +1,9 @@
+const a="Ujjwal";
+const b="rahul";
+const c="Deepak";
+const d="ayush";
+export default b;
+export{a};
+export{c};
+export{d};
+
