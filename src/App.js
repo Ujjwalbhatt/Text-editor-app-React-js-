992273ea0d8aee510092a4a4c131205ab63b27f1
Ujjwal Bhatt <93533269+Ujjwalbhatt@users.noreply.github.com>
2022-08-23 19:38:06 +0530

    react edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title= "Texttutils"/>
-      {/* <Navbar/> */}
+      
+      <div className="container">
+      
+      <TextForm heading="Enter the text to analayaze below"/>
+      
+      </div>
+      
+       {/* <About/> */}
+
+      
     </>
 
   );
