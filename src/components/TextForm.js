9db801df8b64f0afad083a3e901341113402d2d3
Ujjwal Bhatt <93533269+Ujjwalbhatt@users.noreply.github.com>
2022-08-25 08:38:01 +0530

    mywork

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,22 +29,22 @@ export default function TextForm(props) {
     const [text, setText] = useState('Enter the text');
     return (
         <>
-            <div className="container">
-                <h1>{props.heading}</h1>
+            <div className="container" style={{color: props.mode === 'dark'?'white':'#042743'}}>
+                <h1>{props.heading }</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'black':'white', color: props.mode === 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to toLowerCase</button>
                 <button className="btn btn-primary mx-1" onClick={clearscreen}>Clear</button>
                 <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Clear extra spaces</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#042743'}}>
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").length} words & {text.length} characters</p>
                 <p>{0.008 * text.split("").length} Minutes reading time</p>
-                <h3>---------------------preview---------------------------</h3>
-                <p>{text}</p>
+                <h2>preview</h2>
+                <p>{text.length>0?text: "Enter something above to preview the text here"}</p>
             </div>
         </>
     )
